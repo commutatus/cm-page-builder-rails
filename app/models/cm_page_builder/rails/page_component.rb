41160c35f41b7b90d865507a90b20bfec2659369
Camilo Ernesto Forero Junco <camilo.forero3@googlemail.com>
2019-08-28 16:43:31 +0530

    fix: components are now ordered by position

--- app/models/cm_page_builder/rails/page_component.rb
+++ app/models/cm_page_builder/rails/page_component.rb
@@ -1,7 +1,10 @@
 module CmPageBuilder::Rails
   class PageComponent < ApplicationRecord
-    belongs_to :page    
+    belongs_to :page
 
     has_one_attached :component_attachment
+
+    default_scope { order(position: :asc) }
+    
   end
 end
