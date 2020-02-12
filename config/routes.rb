9175baacb2458f2689c3c6a5fa0f1d4f38e8116a
Camilo Forero <camilo.forero3@googlemail.com>
2020-02-12 11:54:45 +0530

    fix: remove unneeded route endpoints and dead code

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 
 
-
-
-
 CmPageBuilder::Rails::Engine.routes.draw do
-  resources :pages
+  # Only individual items can be shown and edited.
+  # The only way to access these is through their parent record in the main application, so no index
+  # Their lifecycle is also associated with their parent record's, so no create and destroy
+  resources :pages, only: [:show, :edit, :update]
 end
